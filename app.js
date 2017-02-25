2da0cca67028f2df61df6ca0bb9d75318de9094d
Lucas Pirola <lucaspirola@gmail.com>
2017-02-24 23:53:51 -0300

    Add postgraphql support

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var express = require('express');
-var app     = express();
+var postgraphql = require('postgraphql').postgraphql;
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+var app = express();
+
+app.use(postgraphql(
+  (process.env.DATABASE_URL || 'postgres://localhost:5432'),
+  (process.env.SCHEMA_NAME || 'public'),
+  (process.env.NODE_ENV === 'production' ? { } : { graphiql: true })
+));
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('pm2-beanstalk listening!');
